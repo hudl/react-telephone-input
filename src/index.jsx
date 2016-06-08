695c2263f08c370ffd826aa966e0b209200f2df0
Ben Holland <ben.holland@loaner67m.agilesports.local>
2016-06-08 11:18:47 -0500

    Didnt cover initial state of number

--- src/index.jsx
+++ src/index.jsx
@@ -474,7 +474,7 @@ var ReactTelephoneInput = React.createClass({
 
     var kickoffClasses = classNames({
       'ko-invalid': this.props.errorMessage,
-      'ko-active': this.state.formattedNumber.split(' ')[1] !== '',
+      'ko-active': this.state.formattedNumber.split(' ').length > 1,
     });
 
     var inputClasses = classNames({
